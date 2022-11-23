6eac58b429cf3e92ba7b2f98d4bc4bc3e69af847
Ruchakale19 <kalerucha19@gmail.com>
2022-11-23 17:28:44 +0530

    PrintShopApi

---
+++ routes/paperMaster-route.js
@@ -0,0 +1,16 @@
+//const router = require('express-promise-router')();
+var  express = require('express');var  express = require('express');
+var  bodyParser = require('body-parser');
+var  cors = require('cors');
+var  app = express();
+var  router = express.Router();
+
+//const { DashboardController } = require('../controllers/basecontroller');
+
+console.log('You are in paper Masters controller')
+const PaperMasterController = require('../controllers/PaperMaster.controller');
+
+
+router.route('/getpaper').get(PaperMasterController.getPaperMaster)
+
+module.exports.router = router;  
